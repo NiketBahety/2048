df5ab9334d7a3900db6a1e5b22921a45d32d53ae
Niket Bahety <niketbahety@gmail.com>
2022-02-25 20:44:26 +0530

    Added All Transitions

--- index.js
+++ index.js
@@ -26,6 +26,17 @@ function getPoints(grid) {
   return array;
 }
 
+function transpose(mat) {
+  for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < i; j++) {
+      const tmp = mat[i][j];
+      mat[i][j] = mat[j][i];
+      mat[j][i] = tmp;
+    }
+  }
+  return mat;
+}
+
 function resizeArray(arr1, arr2) {
   console.table(arr1);
   console.table(arr2);
@@ -42,8 +53,8 @@ function resizeArray(arr1, arr2) {
             k++;
           }
         }
-        console.log(arr1[i]);
-        console.log(arr2[i]);
+        console.log("arr1", arr1[i]);
+        console.log("arr2", arr2[i]);
       }
       for (let j = 0; j < arr1[i].length; j++) {
         let y = 0;
@@ -58,8 +69,69 @@ function resizeArray(arr1, arr2) {
   return x;
 }
 
+function resizeArrayNew(arr1, arr2) {
+  let arr3 = [],
+    arr4 = [];
+
+  for (let k = 0; k < 4; k++) {
+    let arr = [];
+    for (let i = 0; i < 4; i++) {
+      if (arr1[i].length != 0) {
+        for (let j = 0; j < arr1[i].length; j++) {
+          if (arr1[i][j][1] == k) {
+            arr.push(arr1[i][j]);
+          }
+        }
+      }
+    }
+    arr3.push(arr);
+  }
+  for (let k = 0; k < 4; k++) {
+    let arr = [];
+    for (let i = 0; i < 4; i++) {
+      if (arr2[i].length != 0) {
+        for (let j = 0; j < arr2[i].length; j++) {
+          if (arr2[i][j][1] == k) {
+            arr.push(arr2[i][j]);
+          }
+        }
+      }
+    }
+    arr4.push(arr);
+  }
+
+  ///////////
+  let x = [];
+  for (let i = 0; i < 4; i++) {
+    if (arr3[i].length != 0) {
+      if (arr3[i].length === arr4[i].length) console.log(true);
+      else {
+        console.log(false);
+        for (let k = 0; k < arr3[i].length - 1; k++) {
+          if (arr3[i][k][2] === arr3[i][k + 1][2]) {
+            arr4[i].splice(k, 0, arr4[i][k]);
+            k++;
+          }
+        }
+        console.log("arr3", arr3[i]);
+        console.log("arr4", arr4[i]);
+      }
+      for (let j = 0; j < arr3[i].length; j++) {
+        let y = 0;
+        y = Math.abs(arr3[i][j][0] - arr4[i][j][0]) * 124;
+        if (y != 0) y = y * -1;
+        y = y + "%";
+        x.push(y);
+      }
+    }
+  }
+  console.log(x);
+  return x;
+  ///////////
+}
+
 function applyTransitionLeft(x, old) {
-  console.log(old);
+  console.log("old", old);
   console.log(x);
   let count = 0;
   let cells = document.getElementsByClassName("number");
@@ -94,6 +166,70 @@ function applyTransitionRight(x, old) {
         console.log(a);
         console.log(`translateX(${a})`);
         cell.style.transform = `translateX(${a})`;
+        count++;
+      }
+    }
+  }
+  setTimeout(function () {
+    addRandom();
+    createGrid();
+  }, 200);
+}
+
+function applyTransitionUp(x, old) {
+  console.log("old", old);
+
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < i; j++) {
+      let tmp = old[i][j];
+      old[i][j] = old[j][i];
+      old[j][i] = tmp;
+    }
+  }
+
+  console.log("old transpose", old);
+  let count = 0;
+  let cells = document.getElementsByClassName("cell");
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      let cell = cells[4 * j + i];
+      if (old[i][j] != 0) {
+        console.log(cell);
+        console.log(`translateY(${x[count]})`);
+        cell.childNodes[0].style.transform = `translateY(${x[count]})`;
+        count++;
+      }
+    }
+  }
+  setTimeout(function () {
+    addRandom();
+    createGrid();
+  }, 200);
+}
+
+function applyTransitionDown(x, old) {
+  console.log("old", old);
+
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < i; j++) {
+      let tmp = old[i][j];
+      old[i][j] = old[j][i];
+      old[j][i] = tmp;
+    }
+  }
+
+  console.log("old transpose", old);
+  let count = 0;
+  let cells = document.getElementsByClassName("cell");
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      let cell = cells[4 * j + i];
+      if (old[i][j] != 0) {
+        console.log(cell);
+        let a = x[count].split("%")[0] * -1 + "%";
+        console.log(a);
+        console.log(`translateY(${a})`);
+        cell.childNodes[0].style.transform = `translateY(${a})`;
         count++;
       }
     }
@@ -275,6 +411,7 @@ function leftMove(e) {
 
 function rightMove(e) {
   old_grid = grid_num.slice(0);
+  let old_old_grid = grid_num.slice(0);
   let arr = getPoints(old_grid);
   let zero = [];
   let one = [];
@@ -321,7 +458,7 @@ function rightMove(e) {
 
   let x = resizeArray(arr, new_arr);
   console.log(x);
-  applyTransitionRight(x, old_grid);
+  applyTransitionRight(x, old_old_grid);
 
   // addRandom();
   // createGrid();
@@ -329,9 +466,17 @@ function rightMove(e) {
 }
 
 function upMove(e) {
-  old_grid = grid_num;
+  old_grid = grid_num.slice(0);
+  let old_old_grid = [];
+  for (let a = 0; a < 4; a++) {
+    let arr = [];
+    for (let b = 0; b < 4; b++) {
+      arr.push(grid_num[a][b]);
+    }
+    old_old_grid.push(arr);
+  }
+  console.log(old_grid);
   let arr = getPoints(old_grid);
-  console.table(arr);
   let zero = [];
   let one = [];
   for (let i = 0; i < 4; i++) {
@@ -381,17 +526,27 @@ function upMove(e) {
     }
   }
 
-  console.table(getPoints(grid_num));
+  let new_arr = getPoints(grid_num);
+  let x = resizeArrayNew(arr, new_arr);
+  applyTransitionUp(x, old_old_grid);
 
-  addRandom();
-  createGrid();
+  // addRandom();
+  // createGrid();
   document.getElementsByClassName("score")[0].innerHTML = score;
 }
 
 function downMove(e) {
-  old_grid = grid_num;
+  old_grid = grid_num.slice(0);
+  let old_old_grid = [];
+  for (let a = 0; a < 4; a++) {
+    let arr = [];
+    for (let b = 0; b < 4; b++) {
+      arr.push(grid_num[a][b]);
+    }
+    old_old_grid.push(arr);
+  }
+  console.log(old_grid);
   let arr = getPoints(old_grid);
-  console.table(arr);
   let zero = [];
   let one = [];
   for (let i = 0; i < 4; i++) {
@@ -440,10 +595,12 @@ function downMove(e) {
     }
   }
 
-  console.table(getPoints(grid_num));
+  let new_arr = getPoints(grid_num);
+  let x = resizeArrayNew(arr, new_arr);
+  applyTransitionDown(x, old_old_grid);
 
-  addRandom();
-  createGrid();
+  // addRandom();
+  // createGrid();
   document.getElementsByClassName("score")[0].innerHTML = score;
 }
 
